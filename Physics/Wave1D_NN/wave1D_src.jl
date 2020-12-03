ddc244955fd398f465908d8e2792642f706a516c
clee.ted@gmail.com <clee.ted@gmail.com>
2020-12-03 14:06:49 -0600

    wave1D initial value scaling test

--- Physics/Wave1D_NN/wave1D_src.jl
+++ Physics/Wave1D_NN/wave1D_src.jl
@@ -9,27 +9,23 @@ Pkg.instantiate();
 using NeuralPDE, Flux, ModelingToolkit, GalacticOptim, Optim, DiffEqFlux
 using Plots
 
+println("NeuralPDE -- 1D wave equation with source term")
+
 @parameters x, t, θ
+#@parameters xbgn, xend, tlast
 @variables u(..)
 @derivatives Dxx''~x
 @derivatives Dtt''~t
 @derivatives Dt'~t
 
-macro seisrc(x,t)
-    #wvlt_samp = ricker($t, fpeak)
-    #println("Src: fpeak=",fpeak," xbgn,dx,xend=",xbgn,dx,xend)
-    #seisrc = point_source(wvlt_samp, $x, xbgn,dx,xend)
-    return :( println("point_source( ricker($t, fpeak), $x, xbgn,dx,xend)") )
-end
-
-# Build a Ricker wavelet with a given peak frequency (Hz)
+# Build a Ricker wavelet with a given peaNeuralPDE -- 1D wave equation with source termk frequency (Hz)
 function ricker(t, fpeak)
     sigmaInv = pi * fpeak * sqrt(2)
     #cut = 1.e-6
     t0 = 6.0 / sigmaInv
     Δt = t .- t0
     expt = (pi * fpeak .* Δt) .^ 2
-    (1.0 .- 2.0 .* expt) .* exp.(-expt)
+    wv = (1.0 .- 2.0 .* expt) .* exp.(-expt)
 end
 
 # Place a source wavelet sample at a position in space
@@ -38,81 +34,94 @@ function point_source( wvlt_samp, xsrc, xbgn,dx,xend)
     dx = (xend - xbgn)/(nx-1)
     f = zeros(nx, 1)
     # Find the first spatial sample point beyond the source location
+    ixs = 46
+    frac = 0.
+    #=
     xpos = xsrc - xbgn
-    xndx = xpos / dx
-    ixs = max(1, ceil(Int,xndx)) + 1
+    xndx = 1 + trunc( xpos / dx + .5 )
+    println("xpos: ",xpos," dx: ", dx, " xndx: ",xndx)
+    ixs = Int(xndx)
+    #ixs = max(1, ceil(Int,xndx)) + 1
     # Distribute the unit amplitude proportionally
     # between the two neighboring sample positions
     frac = (ixs * dx - xpos) / dx
     #print(frac, ", ", ixs, ", ", xpos)
+    =#
     f[ixs, 1] = (1.0 - frac) * wvlt_samp
     f[ixs-1, 1] = frac * wvlt_samp
-    println("point_source: f=",f)
+    #println("point_source: f=",f)
     f
 end
 
-# macro test
-fpeak = 10.
-xbgn = 0.
-xend = 6000.
-xsrc = 900.
-dx = 20.
-tlast = 3.
-fpeak = 10.
+function seisrc(x,t)
+#=
+    wvlt_samp = ricker(t, fpeak)
+    s = point_source(wvlt_samp, x, xbgn,dx,xend)
+    s
+=#
+    s = 0.
+end
+#@register seisrc(x,t)
 
-mex = @macroexpand( @seisrc( xsrc, .2 ))
-arr = eval(mex)
-arr1 = eval(arr)
-arr1
-arr2 = eval( @seisrc( xsrc, .2 ) )
-arr5
-arr2
-mex
-
-println( arr3[0] )
-point_source( ricker(t, fpeak), x, xbgn,dx,xend)
 #2D PDE
-C=2000.
-eq  = Dtt(u(x,t,θ)) ~ C^2*Dxx(u(x,t,θ)) + @seisrc(x,t)
-
-# Initial and boundary conditions
-bcs = [u(0,t,θ) ~ 0., # for all t > 0
-       u(1,t,θ) ~ 0., # for all t > 0
-       u(x,0,θ) ~ 0., # was: x*(1. - x), #for all 0 < x < 1
-       Dt(u(x,0,θ)) ~ 0. ] #for all  0 < x < 1]
+C= 2. # 1. # 2000.  # 1.
+eqn = Dtt(u(x,t,θ)) ~ C^2*Dxx(u(x,t,θ)) # + seisrc(x,t)
 
 # Space and time domains
-xbgn = 0.
-xend = 6000.
-dx = 20.
+xbgn = 0. ; xend = 6. # 6000.
 tlast = 3.
 fpeak = 10.
-domains = [x ∈ IntervalDomain(xbgn,xend),
-           t ∈ IntervalDomain(0.0,tlast)]
+xdom = x ∈ IntervalDomain(xbgn,xend)
+tdom = t ∈ IntervalDomain(0.0,tlast)
+domains = [xdom,tdom]
+#println(">>>>> domains:...")
+#dump(domains,maxdepth=3)
 
-# Discretization
-dx = 20.
+# Initial and boundary conditions
+bcs = [u(xbgn,t,θ) ~ 0., # for all t > 0
+       u(xend,t,θ) ~ 0., # for all t > 0
+       u(x,0,θ) ~ (x - xbgn)*(xend - x), #  x*(1. - x), # for all 0 < x < 1
+       Dt(u(x,0,θ)) ~ 0. ] # for all x in domain
+#println(">>>>> bcs:...")
+#dump(bcs,maxdepth=3)
+
+# Build a discretized NN struct
+nGrid = 16
+dn = 1. / (nGrid-1)  # Discretization fraction in both x and t domains
+dx = dn * (xdom.domain.upper - xdom.domain.lower)
+xs = xdom.domain.lower:dx:xdom.domain.upper
+dt = round(dn * (tdom.domain.upper - tdom.domain.lower); sigdigits=3 )
+ts = tdom.domain.lower:dt:tdom.domain.upper
+println("xs= ",xs," len ",length(xs),"  ts= ",ts," len ",length(ts))
 
 # Neural network
 chain = FastChain(FastDense(2,16,Flux.σ),FastDense(16,16,Flux.σ),FastDense(16,1))
 
-discretization = PhysicsInformedNN(dx,
-                                   chain,
-                                   strategy= GridTraining())
+discreteNet = PhysicsInformedNN(dn, chain, strategy= GridTraining())
+#println(">>>>> discreteNet:...")
+#dump(discreteNet, maxdepth=3)
 
-pde_system = PDESystem(eq,bcs,domains,[x,t],[u])
-prob = discretize(pde_system,discretization)
+pde_system = PDESystem(eqn, bcs, domains, [x,t], [u] )
+#println(">>>>> pde_system:...")
+#dump(pde_system, maxdepth=3)
+
+prob = discretize(pde_system, discreteNet)
 
 # optimizer
 opt = Optim.BFGS()
-#res = GalacticOptim.solve(prob,opt; cb = cb, maxiters=1200)
-result = GalacticOptim.solve(prob,opt;  maxiters=1200)
-phi = discretization.phi
-# We can plot the predicted solution of the PDE and compare it with the analytical solution in order to plot the relative error.
 
-xs,ts = [domain.domain.lower:dx:domain.domain.upper for domain in domains]
+println(">>>>> Solving...")
+#res = GalacticOptim.solve(prob,opt; cb = cb, maxiters=1200)
+@time result = GalacticOptim.solve(prob, opt;  maxiters=1200)
+phi = discreteNet.phi  # trial solution
+
+# We can plot the predicted solution of the PDE and
+# compare it with the analytical solution in order to plot the relative error.
+println(">>>>> Plotting...")
+
+#xs,ts = [domain.domain.lower:dx:domain.domain.upper for domain in domains]
 
 u_predict = reshape([first(phi([x,t],result.minimizer)) for x in xs for t in ts],(length(xs),length(ts)))
 
-wvPlt =plot(xs, ts, u_predict, linetype=:contourf,title = "predict");
+wvPlt = plot(xs, ts, u_predict, linetype=:contourf, title = "predict");
 plot(wvPlt)
