e87a6508cc52f2e0031545b5c5140a6635536046
Lei Huang <lhuang@pvamu.edu>
2021-02-25 16:03:21 -0600

    update PPO

--- RL/PPO_TrebuchetRL.jl
+++ RL/PPO_TrebuchetRL.jl
@@ -1,10 +1,34 @@
+cd(@__DIR__)
+using Pkg;
+Pkg.activate(".");
+Pkg.instantiate();
+Pkg.precompile();
+
+using ReinforcementLearning
+using Plots
+using Zygote
+using Flux
+using Random
+using IntervalSets
+using StatsPlots
+using BSON: @save, @load
+using StableRNGs
+using Dates
+using Logging
+using TensorBoardLogger: TBLogger
 using Distributions: Categorical, Normal, logpdf
+include("TrebuchetEnv.jl")
+
+output_figures="Figures/"
+output_models="Models/"
+output_data="Data/"
 
 t = Dates.format(now(), "yyyy_mm_dd_HH_MM_SS")
 save_dir = joinpath(pwd(), "checkpoints", "JuliaRL_PPO_Trebuchet_$(t)")
 
 
 lg = TBLogger(joinpath(save_dir, "tb_log"), min_level = Logging.Info)
+seed = 123
 rng = StableRNG(seed)
 inner_env = TrebuchetEnv(continuous = true, rng = rng)
 A = action_space(inner_env)
@@ -61,8 +85,6 @@ agent = Agent(
     ),
 )
 
-stop_condition = StopAfterStep(100_000)
-total_reward_per_episode = TotalBatchRewardPerEpisode(N_ENV)
 hook = ComposedHook(
     total_reward_per_episode,
     DoEveryNStep() do t, agent, env
@@ -84,22 +106,58 @@ hook = ComposedHook(
     end,
 )
 
-run(agent, env, stop_condition, hook)
+function train(agent, hook)
+	stop_condition = StopAfterStep(50_000)
+	total_reward_per_episode = TotalBatchRewardPerEpisode(N_ENV)
+	run(agent, env, stop_condition, hook)
+	pre_model = agent.policy.approximator.actor.pre
+	mu_model = agent.policy.approximator.actor.μ
+	sigma_model = agent.policy.approximator.actor.σ
+	critic_model = agent.policy.approximator.critic
 
-
-env1 = TrebuchetEnv(continuous=true)
-reset!(env1)
-@show state(env1)
-#set_goal!(env, 80)
-rewards = [reward(env1)]
-angles = [env1.state[1]]
-while !is_terminated(env1)
-	action = agent(env1)
-	env1(action)
-	r = reward(env1)
-	@show action, r
-	append!(rewards, r)
-	append!(angles, env1.state[1])
+	@save output_models*"PPO_Trebuchet_actor_pre.bson" pre_model
+	@save output_models*"PPO_Trebuchet_actor_mu.bson" mu_model
+	@save output_models*"PPO_Trebuchet_actor_sigma.bson" sigma_model
+	@save output_models*"PPO_Trebuchet_critic.bson" critic_model
 end
 
-plot([rewards,angles],label=["rewards" "θ"], layout=(2,1))
+function test(agent,hook)
+	@load output_models*"PPO_Trebuchet_actor_pre.bson" pre_model
+	@load output_models*"PPO_Trebuchet_actor_mu.bson" mu_model
+	@load output_models*"PPO_Trebuchet_actor_sigma.bson" sigma_model
+	@load output_models*"PPO_Trebuchet_critic.bson" critic_model
+
+	pre_weights = params(pre_model)
+	mu_weights = params(mu_model)
+	sigma_weights = params(sigma_model)
+	critic_weights = params(critic_model)
+
+	Flux.loadparams!(agent.policy.approximator.actor.pre, pre_weights)
+	Flux.loadparams!(agent.policy.approximator.actor.μ, mu_weights)
+	Flux.loadparams!(agent.policy.approximator.actor.σ, sigma_weights)
+	Flux.loadparams!(agent.policy.approximator.critic, critic_weights)
+
+
+	env1 = TrebuchetEnv(continuous=true)
+	reset!(env1)
+	@show state(env1)
+	#set_goal!(env, 80)
+	rewards = [reward(env1)]
+	angles = [env1.state[1]]
+	while !is_terminated(env1)
+		action = agent(env1)
+		env1(action)
+		r = reward(env1)
+		@show action, r
+		append!(rewards, r)
+		append!(angles, env1.state[1])
+	end
+
+	plot([rewards,angles],label=["rewards" "θ"], layout=(2,1))
+
+end
+
+
+train(agent, hook)
+
+test(agent,hook)
